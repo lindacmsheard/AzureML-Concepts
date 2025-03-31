699f392d1b2db204eae47cd8e78799c74b89fc7c
lindacmsheard <22198777+lindacmsheard@users.noreply.github.com>
2025-03-31 14:03:36 +0100

    clean-up

---
+++ definitions/jobs/bash_job_with_data/src/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get command line named argument
+dir=$1
+
+for f in $dir*.* ; do
+
+    echo $f
+
+    echo
+    echo ====================================
+    echo Processing sample $s
+    echo ====================================
+
+
+done
